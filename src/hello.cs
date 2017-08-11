1b9b4d1fd47d73e2f89ee62f16f554b90dabe4e1
wellflat <wellflat@gmail.com>
2017-08-11 09:40:15 +0900

    changed line terminator

--- src/hello.cs
+++ src/hello.cs
@@ -1,9 +1,9 @@
 using System;
 
 namespace HelloWorld {
-    class Hello {
-        static void Main() {
-            Console.WriteLine("hello, world");
-        }
+  class Hello {
+    static void Main() {
+      Console.WriteLine("hello, world");
     }
+  }
 }
