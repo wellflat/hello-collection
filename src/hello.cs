fdec0eb9ae4ec0f15032dd242266272dcfe89588
wellflat <wellflat@gmail.com>
2017-08-11 23:15:39 +0900

    modified hello.cs

--- src/hello.cs
+++ src/hello.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace HelloWorld {
-  class Hello {
-    static void Main() {
-      Console.WriteLine("hello, world");
-    }
+class Hello {
+  static void Main() {
+    Console.WriteLine("hello, world");
   }
 }
